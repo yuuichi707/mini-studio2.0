23f200d154d5479997ba2eaa6d844c8be48edb07
Vreiym <lrossard@gaming.tech>
2026-03-10 15:44:02 +0100

    have to test camera when we got the player movement, collision with the obstacles and the map

--- mini-studio2.0/mini-studio2.0.cpp
+++ mini-studio2.0/mini-studio2.0.cpp
@@ -4,36 +4,34 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "player.h"
+#include "Camera.h"
+
 int main()
 {
     player rect(0, 0, 0, 0);
 
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
+    Camera camera(800.f, 600.f);
+
+    sf::RectangleShape player(sf::Vector2f(32.f, 32.f));
+    player.setFillColor(sf::Color::Green);
+    player.setPosition(sf::Vector2f(400.f, 300.f));
 
         while (window.isOpen())
         {
-            // Process events
             while (const auto event = window.pollEvent())
             {
-                // Close window: exit
                 if (event->is<sf::Event::Closed>())
                     window.close();
             }
         }
+        sf::Vector2f pos = player.getPosition();
+        camera.update(pos.x, pos.y);
+        window.setView(camera.getView());
+
     rect.draw(window);
 
     window.clear();
 
     window.display();
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
