c59ce6d186709c98ce63510784f546b79f50da20
Fufu-le-Gamer <yducrocq@gaming.tech>
2026-03-13 14:42:10 +0100

    test

--- mini-studio2.0/PlatformsTest.h
+++ mini-studio2.0/PlatformsTest.h
@@ -6,7 +6,7 @@ class PlatformsTest
 public:
 	PlatformsTest(const sf::Vector2f& size,
 		const sf::Vector2f& position,
-		const sf::Color& color = sf::Color::Red);
+		const sf::Color& color = sf::Color::Yellow);
 
 	void draw(sf::RenderWindow& window) const;
 
