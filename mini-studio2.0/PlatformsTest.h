f512d8a03f1acd20c5f1925919dfcdf13ab43719
Fufu-le-Gamer <yducrocq@gaming.tech>
2026-03-13 14:45:26 +0100

    .

---
+++ mini-studio2.0/PlatformsTest.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class PlatformsTest
+{
+public:
+	PlatformsTest(const sf::Vector2f& size,
+		const sf::Vector2f& position,
+		const sf::Color& color = sf::Color::Red);
+
+	void draw(sf::RenderWindow& window) const;
+
+	sf::RectangleShape& shape();
+
+private:
+	sf::RectangleShape m_shape;
+};
+
